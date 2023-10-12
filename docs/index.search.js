39306568705b2d54925243087f2391d4fa008dc6
jacmoe <jacmoe.dk@gmail.com>
2023-10-12 23:16:55 +0200

    Silly mistake fixed

---
+++ docs/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "This is a small test\n",
+    "description": "lorem ipsum",
+    "tags": null,
+    "title": "Welcome to Hotel California of Creative Writing",
+    "uri": "/hotel-california-of-creative-writing/welcome-to-hotel-california-of-creative-writing/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/hotel-california-of-creative-writing/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/hotel-california-of-creative-writing/tags/index.html"
+  }
+]
