931401fca2bdc0cb3ba667ebb8505ea3dd6152bc
jacmoe <jacmoe.dk@gmail.com>
2023-10-12 22:49:57 +0200

    Initial

---
+++ content/_index.md
@@ -0,0 +1,8 @@
++++
+author = ["Jacob Moena"]
+description = "lorem ipsum"
+draft = false
+archetype = "home"
++++
+
+This is a small test
---
+++ docs/index.search.js
@@ -0,0 +1,18 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/brandt/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/brandt/tags/index.html"
+  }
+]
